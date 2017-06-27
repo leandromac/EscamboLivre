804194f41b620afc3278076b663e95c007f8dc21
Leandro Macedo <contato@leandromacedo.com>
2017-06-27 11:46:55 +0100

    Create validation registrations form

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,10 +5,25 @@ class Member < ActiveRecord::Base
 
   has_many :ads
   has_one :profile_member
+
   accepts_nested_attributes_for :profile_member
+
+  # Validations
+  validate :nested_attributes
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def nested_attributes
+     if nested_attributes_blank?
+       errors.add(:base, "É necessário preencher os campos Nome e Sobrenome.")
+     end
+  end
+
+  def nested_attributes_blank?
+    profile_member.first_name.blank? || profile_member.second_name.blank?
+  end
+
 end
